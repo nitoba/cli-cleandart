532f1bb92bfc5a18fc4bf56313fe6aceca8dc04d
Bruno Alves <bruno.santos@ifpi.edu.br>
2020-10-07 11:15:26 -0300

    first commit

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:clean_dart_cli/clean_dart_cli.dart' as clean_dart_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${clean_dart_cli.calculate()}!');
+}
---
+++ test/clean_dart_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:clean_dart_cli/clean_dart_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
